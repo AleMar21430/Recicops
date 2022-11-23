2203ce94967ac3b3698878b99f9d1d96a88710ac
DieggsPapuM <103551254+DieggsPapuM@users.noreply.github.com>
2022-11-23 14:09:23 -0800

    coso

--- app/src/main/java/com/marti21430/recicops/data/adapters/TrashAdapter.kt
+++
@@ -1,7 +0,0 @@
-package com.marti21430.recicops.data.adapters
-
-class TrashAdapter (
-    private val trash: MutableList<Trash>
-        ){
-
-}
--- app/src/main/java/com/marti21430/recicops/data/user/UserAdapter.kt
+++ app/src/main/java/com/marti21430/recicops/data/user/UserAdapter.kt
@@ -1,4 +1,5 @@
 package com.marti21430.recicops.data.user
 
 class UserAdapter {
+
 }

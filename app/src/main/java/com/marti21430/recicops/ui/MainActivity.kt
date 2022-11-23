a97062873a13544abdc1f5bab1608adc99f623c8
AleMar21430 <97700487+Pekoyo@users.noreply.github.com>
2022-11-23 13:13:00 -0600

    Firebase Authentication crash

--- app/src/main/java/com/marti21430/recicops/ui/MainActivity.kt
+++ app/src/main/java/com/marti21430/recicops/ui/MainActivity.kt
@@ -2,13 +2,17 @@ package com.marti21430.recicops.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import com.marti21430.recicops.R
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth = FirebaseAuth.getInstance()
+        auth.createUserWithEmailAndPassword("correotest@gmail.com","testpass123")
     }
 }
--- app/src/main/java/com/marti21430/recicops/ui/core/Register.kt
+++ app/src/main/java/com/marti21430/recicops/ui/core/Register.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.navigation.findNavController
+import com.google.firebase.auth.FirebaseAuth
 import com.marti21430.recicops.R
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -17,7 +18,7 @@ class Register : Fragment(R.layout.fragment_register) {
     private lateinit var password: EditText
     private lateinit var ready: Button
     private lateinit var cancel: Button
-
+    private lateinit var auth: FirebaseAuth;
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -30,18 +31,27 @@ class Register : Fragment(R.layout.fragment_register) {
 
     private fun setListeners() {
         ready.setOnClickListener {
+            auth = FirebaseAuth.getInstance()
+            auth.createUserWithEmailAndPassword(email.text.toString(),password.text.toString()).addOnCompleteListener {
+                if (it.isSuccessful){
+                    Toast.makeText(
+                        requireContext(),getString(R.string.register_success),Toast.LENGTH_LONG
+                    ).show()
+                    CoroutineScope(Dispatchers.IO).launch {
+                        requireContext().dataStore.savePreferencesValue(KEY_USERNAME, email.text.toString())
+                        requireContext().dataStore.savePreferencesValue(KEY_PASSWORD, password.text.toString())
+                        requireContext().dataStore.savePreferencesValue(KEY_LOGOUT, "false")
+                    }
 
-            CoroutineScope(Dispatchers.IO).launch {
-                requireContext().dataStore.savePreferencesValue(KEY_USERNAME, email.text.toString())
-                requireContext().dataStore.savePreferencesValue(KEY_PASSWORD, password.text.toString())
-                requireContext().dataStore.savePreferencesValue(KEY_LOGOUT, "false")
+                    requireView().findNavController().navigate(
+                        RegisterDirections.actionRegisterToLogin()
+                    )
+                }else{
+                    Toast.makeText(
+                        requireContext(),getString(R.string.firebase_register_error),Toast.LENGTH_LONG
+                    ).show()
+                }
             }
-            Toast.makeText(
-                    requireContext(),getString(R.string.register_success),Toast.LENGTH_LONG
-            ).show()
-            requireView().findNavController().navigate(
-                RegisterDirections.actionRegisterToLogin()
-            )
         }
         cancel.setOnClickListener {
             requireView().findNavController().navigate(

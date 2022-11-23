e88e5c796c84c64893a673d2078d4003b1512724
AleMar21430 <97700487+Pekoyo@users.noreply.github.com>
2022-11-23 10:49:33 -0600

    remove recycler

--- app/src/main/java/com/marti21430/recicops/ui/MainActivity.kt
+++ app/src/main/java/com/marti21430/recicops/ui/MainActivity.kt
@@ -2,10 +2,7 @@ package com.marti21430.recicops.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
 import com.marti21430.recicops.R
-import com.marti21430.recicops.ui.core.My_Trash
 
 
 class MainActivity : AppCompatActivity() {
